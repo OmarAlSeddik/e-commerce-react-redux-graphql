00a70a2b23ba3ad57adedd3b6ed33cb8f569b056
unknown <omaralseddik@gmail.com>
2022-08-18 20:50:09 +0200

    More Setup
    Added the global styles and font. Added the favicon and logo images.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Header from "./components/Header";
+import GlobalStyles from "./components/styles/GlobalStyles";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import StyledHeader from "./styles/StyledHeader";
+
+const Header = () => {
+  return <StyledHeader></StyledHeader>;
+};
+
+export default Header;
